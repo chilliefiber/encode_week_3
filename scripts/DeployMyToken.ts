face8a51f20e4ae894531e9793ce4ec839e5ccf9
Your Name <you@example.com>
2025-03-24 00:20:13 +0000

    First commit

---
+++ scripts/DeployMyToken.ts
@@ -0,0 +1,48 @@
+import { createPublicClient, http, createWalletClient, formatEther, toHex } from "viem";
+import { privateKeyToAccount } from "viem/accounts";
+import { sepolia } from "viem/chains";
+import { abi, bytecode } from "../artifacts/contracts/MyToken.sol/MyToken.json";
+import * as dotenv from "dotenv";
+dotenv.config();
+
+const providerApiKey = process.env.ALCHEMY_API_KEY || "";
+const deployerPrivateKey = process.env.PRIVATE_KEY_DEPLOYER || "";
+
+async function main() {
+  const proposals = process.argv.slice(2);
+  if (!proposals || proposals.length < 1)
+    throw new Error("Proposals not provided");
+  const publicClient = createPublicClient({
+    chain: sepolia,
+    transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
+  });
+  const account = privateKeyToAccount(`0x${deployerPrivateKey}`);
+  const deployer = createWalletClient({
+    account,
+    chain: sepolia,
+    transport: http(`https://eth-sepolia.g.alchemy.com/v2/${providerApiKey}`),
+  });
+  console.log("Deployer address:", deployer.account.address);
+  const balance = await publicClient.getBalance({
+    address: deployer.account.address,
+  });
+  console.log(
+    "Deployer balance:",
+    formatEther(balance),
+    deployer.chain.nativeCurrency.symbol
+  );
+  console.log("\nDeploying MyToken contract");
+  const hash = await deployer.deployContract({
+    abi,
+    bytecode: bytecode as `0x${string}`,
+  });
+  console.log("Transaction hash:", hash);
+  console.log("Waiting for confirmations...");
+  const receipt = await publicClient.waitForTransactionReceipt({ hash });
+  console.log("MyToken contract deployed to:", receipt.contractAddress);
+}
+
+main().catch((error) => {
+    console.error(error);
+    process.exitCode = 1;
+  });
